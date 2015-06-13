56b3d2dd592af0423ead709dfacb82778954f4f5
Dan <danwellman@hotmail.com>
2015-06-13 21:21:27 +0100

    Async

--- tasks/get_branchname.js
+++ tasks/get_branchname.js
@@ -8,42 +8,48 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('get_branchname', 'Get the current Git branch name and pass it to another task\'s config', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-        target: null,
-        short: true,
-        quiet: false
-    });
+    grunt.registerMultiTask('get_branchname', 'Get the current Git branch name and pass it to another task\'s config', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var done = this.async(),
+            options = this.options({
+            target: null,
+            short: true,
+            quiet: false
+        });
 
-    var args = [
-        'symbolic-ref', 'HEAD'
-    ];
-
-    if (options.short) {
-        args.push('--short');
-    }
-
-    if (options.quiet) {
-        args.push('-q');
-    }
-
-    grunt.util.spawn({
-        cmd: 'git',
-        args: args
-    }, function (error, result) {
-        if (error) {
-            grunt.log.error([error]);
-            return false;
+        if (!options.target) {
+            grunt.log.error('target option is null, please specify a target');
         }
-        grunt.config.set(options.target, result.stdout);
-    });
 
-  });
+        var args = [
+            'symbolic-ref', 'HEAD'
+        ];
+
+        if (options.short) {
+            args.push('--short');
+        }
+
+        if (options.quiet) {
+            args.push('-q');
+        }
+
+        grunt.util.spawn({
+            cmd: 'git',
+            args: args
+        }, function (error, result) {
+            if (error) {
+                grunt.log.error([error]);
+                return false;
+            }
+            grunt.config.set(options.target, result.stdout);
+            done();
+        });
+
+    });
 
 };
